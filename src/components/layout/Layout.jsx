021d745cf9a88d6509229bbdce1388d4355ac576
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-14 20:06:17 +0100

    inital content push footer

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,4 @@
 import TopBar from "../topbar/TopBar";
-import Footer from "../footer/Footer";
 
 
 export default function Layout({ children }) {
@@ -7,7 +6,7 @@ export default function Layout({ children }) {
       <>
        <TopBar />
         <main>{children}</main>
-        <Footer />
+        
       </>
     )
   }
