f9a06f0943daf4721f5d6a98b11ddaf85bb03468
Masih <masihdon@hotmail.de>
2023-03-14 22:53:13 +0100

    first commit

---
+++ src/components/savedJobs/savedJobs.jsx
@@ -0,0 +1,60 @@
+import Image from 'next/image';
+import styles from "/src/styles/savedJobs.module.css"
+
+
+function Job(obj) {
+
+    const jobs = [
+    {
+      title: 'Skellefteå taxi',
+      desc: 'Taxichaufför',
+      id: 1,
+    img: `/images/image1.png`
+    },
+    {
+      title: 'Northvold',
+      desc: 'Gruppchef',
+      id: 2,
+    img: `/images/image2.png`
+    },
+    {
+      title: 'Skellefteå Lasarett',
+      desc: 'Sjuksköterska',
+      id: 3,
+      img: `/images/image3.png`
+    },
+    {
+      title: 'Skellefteå Kommun',
+      desc: 'Förskolepedagog',
+      id: 4,
+      img: `/images/image4.png`
+    },
+  ];
+
+    return (
+<div className={styles.wrapper}>
+<h2 className={styles.info}>sparade Jobb</h2>
+
+{jobs.map((job) => (
+    
+    <div key={job.id} className={styles.container}>
+        <div className={styles.imgContainer}>
+        <Image src={job.img} alt={job.title} width={120} height={120} className={styles.img} />
+        </div>
+    <div className={styles.jobContainer}>    
+    <h1 className={styles.up}>{job.title}</h1>
+    <p className={styles.down}>{job.desc}</p>
+    </div>
+    <div className={styles.clearBoth}></div>
+    </div>
+))
+}
+</div>
+
+
+    )
+}
+
+
+ export default Job;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+// import Job from '../components/savedJobs/savedJobs'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
