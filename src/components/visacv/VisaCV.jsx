e2bd85a025e6052d1d25c5688ca6746d250fcb07
NishaSaraswat <nisha0217@gmail.com>
2023-03-15 22:34:19 +0100

    icon added for profile page

--- src/components/visacv/VisaCV.jsx
+++ src/components/visacv/VisaCV.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import styles from './VisaCV.module.scss'
-import arrowUp from '../../../public/images/arrow-up-white.svg'
 import Image from 'next/image'
 import {motion} from 'framer-motion'
+import styles from './VisaCV.module.scss'
+import arrowUp from '../../../public/images/arrow-up-white.svg'
 
 export default function VisaCV() {
   return (
     <div className={styles.footer2}>
       <motion.div className={styles.swipeArrow}
-      animate={{y: [0, 0, -5, 0, 0, 0, -5, 0], scale: 0.75}}
-      transition={{ repeat: Infinity, repeatDelay: 4 }}>
+        animate={{y: [0, 0, -5, 0, 0, 0, -5, 0], scale: 0.75}}
+        transition={{ repeat: Infinity, repeatDelay: 4 }}>
         <Image src={arrowUp} alt="up arrow" />
       </motion.div>
       <div>
