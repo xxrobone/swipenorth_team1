597eb66a936b515febca43f5412f25a767fa2d63
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-15 19:26:37 +0100

    inital commit landing page

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,11 +1,25 @@
 import Image from 'next/image'
 import styles from '../landing/Landing.module.css'
+import SwipeUpArrow from '../swipeUpArrow/SwipeUpArrow'
 import backgroundImage from '../../../public/images/backgroundImage.jpg'
+import logo from '../../../public/images/skelleftea_logo_vit.png'
 
-export default function Footer() {
+export default function Landing() {
   return (
-    <div className={styles.imageContainer}>
-      <Image src={backgroundImage} alt="project" />
+    <div className={styles.main}>
+      <div className={styles.logo}>
+        <Image src={logo} alt="logo" width={107} />
+      </div>
+      <div className={styles.overlay}></div>
+      <Image 
+        className={styles.image} 
+        src={backgroundImage} 
+        alt="background" 
+      />
+      <div className={styles.content}>
+        <h1>Välkommen hem!</h1>
+        <SwipeUpArrow />
+      </div>
     </div>
   )
 }
