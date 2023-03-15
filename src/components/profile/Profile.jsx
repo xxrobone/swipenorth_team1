9d763d9c163b2ea287c9000a1a76130ac8b4e94f
NishaSaraswat <nisha0217@gmail.com>
2023-03-15 18:17:31 +0100

    Profile page with image and text done

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,26 +1,35 @@
 import React from 'react'
 import profilepic from 'public/images/profilepic.png'
 import Image from 'next/image'
+import styles from './Profile.module.scss'
+import VisaCV from '../visacv/VisaCV'
 
 export default function Profile() {
   return (
       <>
-    <div>
-        <Image src={profilepic} alt="profile picture" height={314} width={314}/>
-    </div>
-    <div>
-        <div>
-            <p>Elise, 27 år Taxichaufför </p>
+      <div className={styles.main}>
+        <div className={styles.image}>
+            <Image className={styles.profilePic} src={profilepic} alt="profile picture" height={314} width={314} />
         </div>
-        <hr/>
-        <p>Karl är en erfaren och kommunikativ problemlösare med fem års erfarenhet av tidigare 
-           ledartjänster inom den offentliga sektorn samt det ideella föreningslivet. Han har 
-           samtidigt alltid haft ett brett intresse av IT och har ägnat sin fritid åt att utvidga 
-           det intresset. Karl trivs med att samarbeta med andra liksom att driva arbetsuppgifter 
-           på egen hand för att kunna prestera högt och visa goda resultat. På sin senaste arbetsplats 
-           samt i det ideella arbetet har Karl arbetat med projektledning, 
-           resultatorientering och beslutsfattande i myndighetsärenden.</p>
-    </div>
+        <div>
+            <div className={styles.name}>
+               <p><span>Christian</span>, 32 år
+               <br/>
+               Beslutsfattare </p>
+            </div>
+            <hr/>
+            <p>Jag är en erfaren och kommunikativ problemlösare med fem års erfarenhet av tidigare 
+              ledartjänster inom den offentliga sektorn samt det ideella föreningslivet. 
+              Jag har samtidigt alltid haft ett brett intresse av IT och har ägnat min 
+              fritid åt att utvidga det intresset. Jag trivs med att samarbeta med andra 
+              liksom att driva arbetsuppgifter på egen hand för att kunna prestera högt och 
+              visa goda resultat. På min senaste arbetsplats samt i det ideella arbetet har jag 
+              arbetat med projektledning, resultatorientering och beslutsfattande i myndighetsärenden.</p>
+        </div>
+        <div>
+            {/* <VisaCV /> */}
+        </div>
+      </div>
     </>
   )
 }
