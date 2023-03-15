8fd8e29bee645dba8456d962a80dfcc5ceb6e32a
robonexx <robertwagar@gmail.com>
2023-03-15 08:11:05 +0100

    swipeY

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
-import Swipe from '@/components/swipe/Swipe';
-import InfiniteSwipe from '@/components/infiniteSwipe/InfiniteSwipe';
-
+import YaxisSwipe from '@/components/yaxisswipe/YaxisSwipe';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,9 +17,8 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className={styles.main}>
-       {/*  <Swipe /> */}
-        <InfiniteSwipe />
+      <main>
+        <YaxisSwipe />
       </main>
     </>
   );
