80af1f8232053a698d5cfd25882ff79a3925db3c
Masih <masihdon@hotmail.de>
2023-03-16 12:25:22 +0100

    css- optimized for mobile-screen

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,3 +5,4 @@ export default function App({ Component, pageProps }) {
   return (<Component {...pageProps} />
    
 )}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-// import Job from '../components/savedJobs/savedJobs'
+import Job from '../components/savedJobs/savedJobs'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -15,9 +15,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
      <h1>Swipe North clean project</h1>
       </main>
+      
     </>
   )
 }
