ed923ff987140b46204e853747565e3890a6da5c
robonexx <robertwagar@gmail.com>
2023-03-16 08:08:43 +0100

    fixed errors on pages saved, landing and changed page name to landingpage

---
+++ src/pages/savedjobspage/index.js
@@ -0,0 +1,11 @@
+import SavedJobs from '@/components/savedJobs/savedJobs.jsx';
+
+export default function SavedJobsPage() {
+  return (
+    <>
+      <main>
+        <SavedJobs />
+      </main>
+    </>
+  );
+}
