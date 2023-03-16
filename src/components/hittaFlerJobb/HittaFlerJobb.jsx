979403a768085d755b3cd096db5be0efe2b0d00d
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-16 14:56:48 +0100

    change arrow color to white

--- src/components/hittaFlerJobb/HittaFlerJobb.jsx
+++ src/components/hittaFlerJobb/HittaFlerJobb.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import styles from './HittaFlerJobb.module.scss'
 import arrowUp from '../../../public/images/arrow-up-white.svg'
-import SwipeUpArrow from '../swipeUpArrow/SwipeUpArrow'
 
 export default function HittaFlerJobb() {
   return (
     <div className={styles.footer2}>
-      <SwipeUpArrow />
+      <motion.div className={styles.swipeArrow}
+        animate={{y: [0, 0, -5, 0, 0, 0, -5, 0], scale: 0.75}}
+        transition={{ repeat: Infinity, repeatDelay: 4 }}>
+        <Image src={arrowUp} alt="up arrow" />
+      </motion.div>
       <div className={styles.hitta}>
         <h2>Hitta fler jobb</h2>
       </div>
