9dd99a16b257103e3f2da629f27513f34ec24664
alisiia2601 <alisiia.ryzhykova@icloud.com>
2023-03-17 01:05:43 +0100

    functionality

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,27 +3,34 @@ import styles from '../header/Header.module.css'
 import logoVit from '../../../public/images/skelleftea_logo_vit.png'
 import iconVector from '../../../public/images/Vector.png'
 import iconEllipse from '../../../public/images/ellipse.png'
+import Link from 'next/Link'
 
 
 export default function Header() {
   return (
     <div className={styles.headerContainer}>
       <div className={styles.row}>
+        <Link href="/landingpage">
         <span className={styles.headerLogo}>
-          <Image src={logoVit} alt="logo" width={85} />
+          <Image src={logoVit} alt="logo" width={107} />
         </span>
+        </Link>
       </div>
 
       <div className={styles.row}>
+        <Link href="/savedjobspage">
         <span className={styles.headerIcon1}>
           <Image src={iconVector} alt="logo" width={27} />
         </span>
+        </Link>
       </div>
 
       <div className={styles.row}>
+        <Link href="/profilepage">
         <span className={styles.headerIcon2}>
           <Image src={iconEllipse} alt="logo" width={25} />
         </span>
+        </Link>
       </div>
       
     </div>
