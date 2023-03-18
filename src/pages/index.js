366600c6d1b7e1198be1822cacc6f02de8fd8ea1
Masih <masihdon@hotmail.de>
2023-03-18 14:49:49 +0100

    Your commit message here

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,8 @@ import Image from 'next/image'
 import Job from '../components/savedJobs/savedJobs'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Head from 'next/head';
-import { Inter } from 'next/font/google';
 import YaxisSwipe from '@/components/yaxisswipe/YaxisSwipe';
-import styles from '@/styles/Home.module.css';
+
 import Landing from '@/components/landing/Landing'
 import Footer from '@/components/footer/Footer';
 
